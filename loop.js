d46aa40bc8772e6bbeed4c5957fddcb7604c2b09
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-11-19 17:42:40 +0600

    loop.js

--- loop.js
+++ loop.js
@@ -1,4 +1,14 @@
 // what is Javascript Loop?
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+// for (let i = 0; i <= 100; i++) {
+//     console.log(i);
+// }
+
 
 // for Loop
 
